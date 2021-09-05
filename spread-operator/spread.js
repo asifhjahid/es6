386438607d928e436e4cd63253d448d4896b08e8
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-05 23:46:38 +0600

    spread operator

---
+++ spread-operator/spread.js
@@ -0,0 +1,12 @@
+let num1 = [10,20,30];
+let num2 = [2,3,4];
+let num3 = [40,50,60];
+let allNumbers =[...num1,...num2,5,...num3];
+console.log(allNumbers);
+
+
+//maximum value 
+
+let values = [300,400,200,500,8000,6000,];
+let result = Math.max(...values);
+console.log(result);
