de3ed0bda6289c9e2611a35e291b34b5bbf2f035
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-06 23:56:54 +0600

    splice

--- all-array-methods/filter.js
+++ all-array-methods/filter.js
@@ -2,9 +2,10 @@ let numbers = [2,3,5,1,5,6,9,10,15,30,20];
 
 let number = numbers.filter((num,index,arr) => {
       
-    //   console.log(index);
-    //   console.log(arr);
-      return num>9;
+      console.log(index);
+      console.log(arr);
+      return num>9 
+
 });
 
 console.log(number);
---
+++ all-array-methods/splice.js
@@ -0,0 +1,6 @@
+let numbers = [2,3,5,1,5,6,9,10,15,30,20];
+
+let number = numbers.splice(1,6,40,50);
+
+console.log(number); //deleted elements
+console.log(numbers);
