13675c5763e24406889fd8cfd4081f7955d56637
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-07 03:04:15 +0600

    updated destructuring

--- destructuring/destructure.js
+++ destructuring/destructure.js
@@ -14,6 +14,7 @@ const person ={
 
 const {name1,salary} = person;
 const {address,phone} = person.info;
+// const {info:{address},info:{phone}} = person;
 console.log(name1,salary);
 console.log(address,phone);
 
@@ -21,5 +22,19 @@ console.log(address,phone);
 //array destructuring
 
 const person2 = ['asif','hasan','jahid','dhrubo'];
+
 const[firstName,...nextAllNames]=person2;
+
+const [,secondIndex,,lastIndex] = person2; //print hasan and dhrubo
+
+console.log(secondIndex,lastIndex);
 console.log(firstName,nextAllNames);
+
+
+
+//value swap
+
+let a = 10;
+let b = 20;
+[b,a] = [a,b];
+console.log(a,b);
