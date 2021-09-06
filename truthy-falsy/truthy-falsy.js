23bddac1641c65ed2e9d5a37029263ec181ec9b2
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-06 12:40:33 +0600

    truthy or falsy values

---
+++ truthy-falsy/truthy-falsy.js
@@ -0,0 +1,16 @@
+
+//falsy values => 0, null,NaN, undefined,'',false;
+
+// let number = 0;
+// let number = null;
+// let number = NaN;
+// let number = undefined;
+// let number = '';
+let number = false;
+
+if(number){
+    console.log(`Condition is true`);
+}
+else{
+    console.log(`Condition is false`);
+}
