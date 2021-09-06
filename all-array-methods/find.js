86f42a876071d64f0b649867cee7005df9ffa0ca
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-06 23:23:55 +0600

    find

---
+++ all-array-methods/find.js
@@ -0,0 +1,15 @@
+
+//array.find()
+
+let numbers = [2,3,5,1,5,6,9,10,15];
+
+let result = numbers.find(num=> num>9);
+
+console.log(numbers);
+console.log(result);
+
+
+//array findIndex
+
+let result2= numbers.findIndex(num=> num>9);
+console.log(result2);
