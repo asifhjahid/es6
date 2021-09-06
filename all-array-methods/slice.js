55723e9bfb5c88b6aa6183038252876585ceed67
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-06 23:43:22 +0600

    slice

---
+++ all-array-methods/slice.js
@@ -0,0 +1,9 @@
+let numbers = [2,3,5,1,5,6,9,10,15,30,20];
+
+let number = numbers.slice(-4,-1);
+// let newNumber = numbers.slice();
+
+console.log(number);
+console.log(numbers);
+
+// console.log(newNumber);
