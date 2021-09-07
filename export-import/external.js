813274915cb68963e7596653c3892ec88ceaaefd
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-07 14:03:41 +0600

    export-import

---
+++ export-import/external.js
@@ -0,0 +1,6 @@
+import  myAge,{name} from "./internal.js";  // named import
+// import { name as varName,age as varAge} from "./internal.js";  // named import
+
+// import * as all from './internal.js'
+
+console.log(name,myAge);
---
+++ export-import/internal.js
@@ -0,0 +1,4 @@
+ export let name = 'Asif';
+ let age = 26
+ export default age;
+
