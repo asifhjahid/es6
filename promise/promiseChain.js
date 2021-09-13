2c49897811b77dba55c7f0ca7d50c85e8aa44ae8
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-13 10:59:14 +0600

    promise chainig

---
+++ promise/promiseChain.js
@@ -0,0 +1,35 @@
+const taskOne = ()=>{
+    return new Promise((resolve,reject)=>{
+        resolve(`Task 1 is completed`)
+    })
+}
+
+const taskTwo = ()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(`Task 2 is completed`)
+        },3000)
+    })
+}
+
+const taskThree = ()=>{
+    return new Promise((resolve,reject)=>{
+        reject(`Task 3 is not completed`)
+    })
+}
+
+const taskFour = ()=>{
+    return new Promise((resolve,reject)=>{
+        resolve(`Task 4 is completed`)
+    })
+}
+
+taskOne()
+.then(res=>console.log(res))
+.then(taskTwo)
+.then(res=>console.log(res))
+.then(taskThree)
+.then(res=>console.log(res))
+.then(taskFour)
+.then(res=>console.log(res))
+.catch(err=>console.log(err))
